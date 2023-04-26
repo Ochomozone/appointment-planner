a9fbe5d69c7bae4ffab1ff9b97fba5b71d4eee4a
Ochomozone <112177400+Ochomozone@users.noreply.github.com>
2023-04-26 13:30:58 +0300

    Form added to contact form.js

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,16 +1,28 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm =(props) => { 
+  const handleNameChange = ({target}) => {
+    const [name, value] =target;
+    props.setName(value);
+  };
+
+  const handlePhoneChange = ({target}) => {
+    const [name, value] =target;
+    props.setPhone(value);
+  };
+
+  const handleEmailChange = ({target}) => {
+    const [name, value] =target;
+    props.setEmail(value);
+  };
+
   return (
-    <></>
+
+    <form onSubmit={props.handleSubmit}>
+      <input type='text' label='Name' placeholder='Enter Name' value={props.name} onChange={handleNameChange} />
+      <input type='text' label='Phone' placeholder='Enter Phone' pattern='/^(?:\+?\d{1,3}|\b0)\s?\d{3}\s?\d{3}\s?\d{3}$/' value={props.phone} onChange={handlePhoneChange} />
+      <input type='text' label='Email' placeholder='Enter Email' value={props.email} onChange={handleEmailChange} />
+    </form>
   );
 };
 
